2aac53aacf5e2bd08d156986f96c4626a9930be6
eunyeong-so <eunswd@gmail.com>
2021-05-23 22:22:38 +0900

    Add unity file

---
+++ Assets/Scripts/WeatherConditionInfo.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class WeatherContionInfo
+{
+    public Coord coord { get; set; }
+    public List<Weather> weather { get; set; }
+    public string @base { get; set; }
+    public Main main { get; set; }
+    public int visibility { get; set; }
+    public Wind wind { get; set; }
+    public Clouds clouds { get; set; }
+    public int dt { get; set; }
+    public Sys sys { get; set; }
+    public int timezone { get; set; }
+    public int id { get; set; }
+    public string name { get; set; }
+    public int cod { get; set; }
+
+
+    public int GetCelsius()
+    {
+        //Origin data is kelvin
+        //292.73K − 273.15 = 19.58°C
+
+        return Mathf.RoundToInt(main.temp - 273.15f);
+    }
+
+    public string GetWeatherIcon()
+    {
+        string iconNumber = weather[0].icon;
+        string imageUrl = " http://openweathermap.org/img/wn/" + iconNumber + "@2x.png";
+
+        return imageUrl;
+    }
+}
+
+
+public class Coord
+{
+    public double lon { get; set; }
+    public double lat { get; set; }
+}
+
+public class Weather
+{
+    public int id { get; set; }
+    public string main { get; set; }
+    public string description { get; set; }
+    public string icon { get; set; }
+}
+
+public class Main
+{
+    public float temp { get; set; }
+    public double feels_like { get; set; }
+    public double temp_min { get; set; }
+    public double temp_max { get; set; }
+    public int pressure { get; set; }
+    public int humidity { get; set; }
+    public int sea_level { get; set; }
+    public int grnd_level { get; set; }
+}
+
+public class Wind
+{
+    public double speed { get; set; }
+    public int deg { get; set; }
+    public double gust { get; set; }
+}
+
+public class Clouds
+{
+    public int all { get; set; }
+}
+
+public class Sys
+{
+    public int type { get; set; }
+    public int id { get; set; }
+    public string country { get; set; }
+    public int sunrise { get; set; }
+    public int sunset { get; set; }
+}
+ 
+
---
+++ Assets/Scripts/WeatherData.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using Newtonsoft.Json;
+using UnityEngine.UI;
+
+public class WeatherData : MonoBehaviour
+{
+    private const string apiKey = "037a6b873a377d77791581aad7e067fe";
+    public string cityName = "seoul";
+
+   
+    private void Start()
+    {
+        string url = String.Format("api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", cityName, apiKey);
+       
+        StartCoroutine(GetWeather(url));
+
+    }
+
+    IEnumerator GetWeather(string url)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+
+            yield return webRequest.SendWebRequest();
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Web reqeust error" + webRequest.error);
+
+            }
+            else
+            {
+                var weather = JsonConvert.DeserializeObject<WeatherContionInfo>(webRequest.downloadHandler.text);
+
+                Debug.Log("weather id: " + weather.name);
+                Debug.Log("weather description: " + weather.weather[0].description);
+                Debug.Log("weather Icon: " + weather.weather[0].icon);
+                Debug.Log("weather temp: " + weather.GetCelsius());
+
+            }
+
+        }
+
+    }
+
+}
+ 
